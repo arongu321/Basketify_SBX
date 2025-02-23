2f327bb1d3ac7ad5516b4d736e374f6dd34d6dcf
Zachary Schmidt <zacharyaschmidt@hotmail.ca>
2025-02-22 18:39:07 -0700

    Set up test routes for new pages: login & search, extra views

--- test_server/backend/backend/urls.py
+++ test_server/backend/backend/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('home.urls')),
+    path('api/login-message/', include('home.urls')),
+    path('api/search-message/', include('home.urls')),
 ]
 
--- test_server/backend/home/views.py
+++ test_server/backend/home/views.py
@@ -1,4 +1,15 @@
 from django.http import JsonResponse
+import datetime
 
 def welcome(request):
     return JsonResponse({'message': 'Welcome to Django with React!'})
+
+def get_login_message(request):
+    now = datetime.datetime.now()
+    message = f"Welcome to the Login Page! Current time from Django: {now}"
+    return JsonResponse({"message": message})
+
+def get_search_message(request):
+    now = datetime.datetime.now()
+    message = f"Welcome to the Login Page! Current time from Django: {now}"
+    return JsonResponse({"message": message})
